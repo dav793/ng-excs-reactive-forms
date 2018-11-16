9a03b78b3d780b0cf1aa957a534b87acdbcf5741
David Vargas <dav793@gmail.com>
2018-11-16 14:12:16 -0600

    ready

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, FormArray, FormBuilder, Validators} from '@angular/forms';
-
-import { DietaryPreferenceValidationService } from '../shared/services/dietary-preference-validation.service';
 
 import { Contact, Address } from '../shared/models/contact.model';
-import { legalAgeValidator } from '../shared/validators/age-range.validator';
-import { DietaryPreferenceValidator } from '../shared/validators/dietary-preference.validator';
 
 /**
  * Guía de trabajo:
@@ -34,8 +29,6 @@ import { DietaryPreferenceValidator } from '../shared/validators/dietary-prefere
 })
 export class ContactFormComponent implements OnInit {
 
-  name = new FormControl('Daniel');
-
   // modelo que viene del servidor
   model = new Contact({
     fullName: 'Ace Ventura',
@@ -63,101 +56,8 @@ export class ContactFormComponent implements OnInit {
   dietaryPrefOpts = Contact.DietaryPreferences;
   ageRangeOpts = Contact.AgeRanges;
 
-  // formulario para el DOM, creado "a mano" (4)
-  form = this.createForm(this.model);
-
-  // formulario para el DOM, creado con FormBuilder (4)
-  form2 = this.createFormWithBuilder(this.model);
-
-  constructor(
-    private dietValidationService: DietaryPreferenceValidationService,
-    private formBuilder: FormBuilder
-  ) {
-
-
-    this.name.setValue('Roberto');
-
-    this.name.valueChanges.subscribe((newValue) => {
-      console.log('cambios en control', newValue);
-    });
-
-    this.name.setValue('Pedro', {emitEvent: false});
-
-
-    this.form2.valueChanges.subscribe((newValue) => {
-      console.log('cambios en group', newValue);
-    });
-
-  }
+  constructor() {}
 
   ngOnInit() {}
-
-  createForm(model: Contact): FormGroup {
-    return new FormGroup({
-      fullName:             new FormControl(model.fullName, [Validators.required]),
-      email:                new FormControl(model.email, [Validators.email]),
-      dietaryPreferences:   new FormControl(model.dietaryPreferences),
-      isMarried:            new FormControl(model.isMarried),
-      ageRange:             new FormControl(model.ageRange)
-    });
-  }
-
-  createFormWithBuilder(model: Contact): FormGroup {
-
-    // return this.formBuilder.group(model);
-
-    const addressesGroups = this.setAddresses(model.addresses);
-
-    const group = this.formBuilder.group({
-      fullName:             [ model.fullName,             [Validators.required] ],
-      email:                [ model.email,                [Validators.email] ],
-      dietaryPreferences:   [ model.dietaryPreferences,   [], [DietaryPreferenceValidator.createValidator(this.dietValidationService)] ],
-      isMarried:            [ model.isMarried,            [] ],
-      ageRange:             [ model.ageRange,             [legalAgeValidator()] ],
-      addresses:            this.formBuilder.array(addressesGroups)
-    });
-    // dietaryPreferenceValidator
-
-    return group;
-
-  }
-
-  get addresses() {
-    return this.form2.get('addresses') as FormArray;
-  }
-
-  setAddresses(addresses: Address[]) {
-    const addressesGroups = addresses.map((addr: Address) => {
-      return this.formBuilder.group({
-        building:     [ addr.building,    [] ],
-        street:       [ addr.street,      [] ],
-        city:         [ addr.city,        [] ],
-        zip:          [ addr.zip,         [Validators.pattern('^[0-9]{5}$')] ]
-      });
-    });
-    return addressesGroups;
-  }
-
-  addAddress() {
-    const addr = new Address({});
-    this.addresses.push(this.formBuilder.group({
-      building:     [ addr.building,    [] ],
-      street:       [ addr.street,      [] ],
-      city:         [ addr.city,        [] ],
-      zip:          [ addr.zip,         [Validators.pattern('^[0-9]{5}$')] ]
-    }));
-    this.form2.markAsDirty();
-  }
-
-  removeAddress(index: number) {
-    const addressesCopy: Address[] = Object.assign([], this.form2.value.addresses);
-    addressesCopy.splice(index, 1);
-
-    const addressesGroups = this.setAddresses(addressesCopy);
-    const addressesFormArray = this.formBuilder.array(addressesGroups);
-
-    this.form2.setControl('addresses', addressesFormArray);
-    this.form2.markAsDirty();
-  }
 
 }
--- src/app/shared/validators/age-range.validator.ts
+++
@@ -1,9 +0,0 @@
-import { AbstractControl, ValidatorFn } from '@angular/forms';
-
-export function legalAgeValidator(): ValidatorFn {
-  return (control: AbstractControl): {[key: string]: any} | null => {
-    if (control.value !== '0-18')
-      return null;
-    return {'legalAge': {value: control.value}};
-  };
-}
